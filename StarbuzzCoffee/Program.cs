d3fc7fa5c4ff527a4c5001a04e14baefc15aacf0
ait27ru <ait27ru@gmail.com>
2017-07-01 19:18:57 +1200

    Add project files.

---
+++ StarbuzzCoffee/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarbuzzCoffee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
